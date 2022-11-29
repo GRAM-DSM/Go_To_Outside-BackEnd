7e42f327dd76228eb941d0aa677494a4d3a3833b
gilgeunwoo <kesksjxhx@gmail.com>
2022-11-29 18:22:41 +0900

    프로젝트 생성

---
+++ src/main/java/com/example/gotooutside/GotooutsideApplication.java
@@ -0,0 +1,13 @@
+package com.example.gotooutside;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GotooutsideApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GotooutsideApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gotooutside/GotooutsideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gotooutside;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GotooutsideApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
