76017bbf475332b3c83287bb8633c0684a41060c
gilgeunwoo <kesksjxhx@gmail.com>
2022-11-30 23:06:09 +0900

    .//

--- src/main/java/com/example/gotooutside/domain/auth/domain/RefreshToken.java
+++ src/main/java/com/example/gotooutside/domain/auth/domain/RefreshToken.java
@@ -1,8 +1,8 @@
 package com.example.gotooutside.domain.auth.domain;
 
-import jakarta.persistence.Id;
 import lombok.Builder;
 import lombok.Getter;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.redis.core.RedisHash;
 import org.springframework.data.redis.core.TimeToLive;
 
--- src/main/java/com/example/gotooutside/domain/auth/domain/repository/RefreshTokenRepository.java
+++ src/main/java/com/example/gotooutside/domain/auth/domain/repository/RefreshTokenRepository.java
@@ -2,6 +2,7 @@ package com.example.gotooutside.domain.auth.domain.repository;
 
 import com.example.gotooutside.domain.auth.domain.RefreshToken;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
-public interface RefreshTokenRepository extends JpaRepository<RefreshToken, String> {
+public interface RefreshTokenRepository extends CrudRepository<RefreshToken, String> {
 }
