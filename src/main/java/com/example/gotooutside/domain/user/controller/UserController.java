94961703a657b59bef540c198ce983f4c33e1237
gilgeunwoo <kesksjxhx@gmail.com>
2022-11-30 21:45:41 +0900

    선생님 회원가입 controller

--- src/main/java/com/example/gotooutside/domain/user/controller/UserController.java
+++ src/main/java/com/example/gotooutside/domain/user/controller/UserController.java
@@ -1,7 +1,9 @@
 package com.example.gotooutside.domain.user.controller;
 
 import com.example.gotooutside.domain.user.controller.dto.request.StudentSignUpRequest;
+import com.example.gotooutside.domain.user.controller.dto.request.TeacherSignUpRequest;
 import com.example.gotooutside.domain.user.service.StudentSignUpService;
+import com.example.gotooutside.domain.user.service.TeacherSignUpService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,10 +17,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     private final StudentSignUpService studentSignUpService;
+    private final TeacherSignUpService teacherSignUpService;
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/student/signup")
     public void studentSignUp(StudentSignUpRequest request) {
         studentSignUpService.execute(request);
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("/teacher/signup")
+    public void teacherSigUp(TeacherSignUpRequest request) {
+        teacherSignUpService.execute(request);
     }
 }
