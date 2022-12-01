2664a3568f29030635eec39e3edbeeca66de39b2
gilgeunwoo <kesksjxhx@gmail.com>
2022-12-01 22:18:18 +0900

    passRepository

--- src/main/java/com/example/gotooutside/domain/pass/domain/repository/PassRepository.java
+++ src/main/java/com/example/gotooutside/domain/pass/domain/repository/PassRepository.java
@@ -4,7 +4,10 @@ import com.example.gotooutside.domain.pass.domain.Pass;
 import com.example.gotooutside.domain.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PassRepository extends JpaRepository<Pass, Long> {
 
+    List<Pass> findAllByOrderByCreatedAtDesc();
     boolean existsByUser(User user);
 }
