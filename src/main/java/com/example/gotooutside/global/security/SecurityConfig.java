c9ff8f9bca1be37120db98fecb0d457e2a340d10
gilgeunwoo <kesksjxhx@gmail.com>
2022-12-01 22:19:07 +0900

    api 권한 처리

--- src/main/java/com/example/gotooutside/global/security/SecurityConfig.java
+++ src/main/java/com/example/gotooutside/global/security/SecurityConfig.java
@@ -1,10 +1,12 @@
 package com.example.gotooutside.global.security;
 
+import com.example.gotooutside.global.enums.Authority;
 import com.example.gotooutside.global.security.jwt.JwtTokenProvider;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -34,7 +36,17 @@ public class SecurityConfig {
                 .and()
                 .authorizeRequests()
 
-                .anyRequest().permitAll()
+                //USER
+                .antMatchers(HttpMethod.POST, "/user/**").permitAll()
+
+                //Pass
+                .antMatchers(HttpMethod.GET, "pass/**").hasAnyAuthority()
+                //student
+                .antMatchers(HttpMethod.POST, "/pass").hasAuthority(Authority.STUDENT.name())
+                //teacher
+                .antMatchers(HttpMethod.GET, "/pass").hasAuthority(Authority.TEACHER.name())
+                .antMatchers(HttpMethod.PATCH, "/pass/**").hasAuthority(Authority.TEACHER.name())
+                .anyRequest().authenticated()
 
                 .and()
                 .apply(new FilterConfig(jwtTokenProvider, objectMapper))
